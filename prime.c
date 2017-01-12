d2928a79eeca75a2966d2eb9438c3807f6eebf91
NaguruHarshavardhanreddy <naguruharshareddy@gmail.com>
2017-01-12 14:48:44 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,26 @@
+# enrich16-17
+#include<stdio.h>
+int main()
+{
+   int n,i,j,flag=0;
+   print("enter the number\n");
+   scanf("%d",&n);
+   for(i=n+1;i<=200;i++)
+   {
+      flag = 0;
+      for(j=2;j<i;j++)
+      {
+         if(i%j==0)
+         {
+            flag = 1;
+            break;
+         }
+       }
+       if(flag==0)
+       {
+          printf("the next prime is %d",i);
+       }
+   }
+   return 0;
+}
+        
