9288def95343a5ebf42ca21a7f114049e4baa5d4
NaguruHarshavardhanreddy <naguruharshareddy@gmail.com>
2017-01-12 21:00:55 +0530

    Create traingles.c

---
+++ traingles.c
@@ -0,0 +1,26 @@
+# enrich16-17
+nt main()
+{
+    int n;
+    int i,j;
+    int a[20][20]={0};
+    printf("enter the number\n");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=0;j<i;j++)
+        {
+            if(j==0 || j==n-1 )
+            {
+                a[i][j]=1;
+            }
+            else
+            {
+                a[i][j]=a[i-1][j-1]+a[i-1][j];
+            }
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
